d602ae17824111d4723f3a7285b33a5cd51d3ba1
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2017-03-09 00:16:19 +0200

    Mad fix of whole thing to work with other changes ;)

--- sniffer.py
+++ sniffer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 __author__    = "ktown"
 __copyright__ = "Copyright Adafruit Industries 2014 (adafruit.com)"
 __license__   = "MIT"
@@ -7,19 +9,21 @@ import os
 import sys
 import time
 import argparse
+import subprocess
 
-from SnifferAPI import Logger
 from SnifferAPI import Sniffer
 from SnifferAPI import CaptureFiles
 from SnifferAPI.Devices import Device
 from SnifferAPI.Devices import DeviceList
-
+from SnifferAPI.PcapPipe import PcapPipe
 
 mySniffer = None
 """@type: SnifferAPI.Sniffer.Sniffer"""
 
+myPipe = None
+"""@type: PcapPipe.PcapPipe"""
 
-def setup(serport, delay=6):
+def setup(serport, delay=1):
     """
     Tries to connect to and initialize the sniffer using the specific serial port
     @param serport: The name of the serial port to connect to ("COM14", "/dev/tty.usbmodem1412311", etc.)
@@ -30,7 +34,7 @@ def setup(serport, delay=6):
     global mySniffer
 
     # Initialize the device on the specified serial port
-    print "Connecting to sniffer on " + serport
+    print("Connecting to sniffer on " + serport)
     mySniffer = Sniffer.Sniffer(serport)
     # Start the sniffer
     mySniffer.start()
@@ -46,7 +50,7 @@ def scanForDevices(scantime=5):
     @rtype: DeviceList
     """
     if args.verbose:
-        print "Starting BLE device scan ({0} seconds)".format(str(scantime))
+        print("Starting BLE device scan ({0} seconds)".format(str(scantime)))
 
     mySniffer.scan()
     time.sleep(scantime)
@@ -65,19 +69,19 @@ def selectDevice(devlist):
     count = 0
 
     if len(devlist):
-        print "Found {0} BLE devices:\n".format(str(len(devlist)))
+        print("Found {0} BLE devices:\n".format(str(len(devlist))))
         # Display a list of devices, sorting them by index number
         for d in devlist.asList():
             """@type : Device"""
             count += 1
-            print "  [{0}] {1} ({2}:{3}:{4}:{5}:{6}:{7}, RSSI = {8})".format(count, d.name,
+            print("  [{0}] {1} ({2}:{3}:{4}:{5}:{6}:{7}, RSSI = {8})".format(count, d.name,
                                                                              "%02X" % d.address[0],
                                                                              "%02X" % d.address[1],
                                                                              "%02X" % d.address[2],
                                                                              "%02X" % d.address[3],
                                                                              "%02X" % d.address[4],
                                                                              "%02X" % d.address[5],
-                                                                             d.RSSI)
+                                                                             d.RSSI))
         try:
             i = int(raw_input("\nSelect a device to sniff, or '0' to scan again\n> "))
         except KeyboardInterrupt:
@@ -94,22 +98,69 @@ def selectDevice(devlist):
             # This will start a new scan
             return None
 
+def loop():
+    """Main loop printing some useful statistics"""
+    nLoops = 0
+    nPackets = 0
+    connected = False
 
-def dumpPackets():
-    """Dumps incoming packets to the display"""
-    # Get (pop) unprocessed BLE packets.
-    packets = mySniffer.getPackets()
-    # Display the packets on the screen in verbose mode
-    if args.verbose:
-        for packet in packets:
-            if packet.blePacket is not None:
-                # Display the raw BLE packet payload
-                # Note: 'BlePacket' is nested inside the higher level 'Packet' wrapper class
-                print packet.blePacket.payload
-            else:
-                print packet
+    while True:
+        time.sleep(0.1)
+
+        packets   = mySniffer.getPackets()
+        nLoops   += 1
+        nPackets += len(packets)
+
+        if args.verbose:
+            for packet in packets:
+                if packet.blePacket is not None:
+                    # Display the raw BLE packet payload
+                    # Note: 'BlePacket' is nested inside the higher level 'Packet' wrapper class
+                    print(str(packet.blePacket.payload))
+                else:
+                    print(str(packet))
+        else:
+            '''
+            for packet in packets:
+                if packet.valid:
+                    sys.stdout.write('.')
+                else:
+                    sys.stdout.write('x')
+            sys.stdout.write('\n')
+            '''
+            if connected != mySniffer.inConnection or nLoops % 20 == 0:
+                connected = mySniffer.inConnection
+                sys.stdout.write("\rconnected: {}, packets: {}, missed: {}".format(mySniffer.inConnection, nPackets, mySniffer.missedPackets))
+                sys.stdout.flush()
+
+
+def setupPipe():
+    """setup pipe"""
+    # Create a named pipe for Wireshark capture
+    myPipe = PcapPipe('ble.pipe')
+
+    if sys.platform == 'win32':
+        ws_app = r'C:\Program Files (x86)\Wireshark\Wireshark.exe'
+        ws_app = r'C:\Program Files\Wireshark\Wireshark.exe'
+        ws_app = r'C:\Users\volodymyr.shymanskyy\Downloads\WiresharkPortable\WiresharkPortable.exe'
+        ws_app = '"' + ws_app + '"'
     else:
-        print '.' * len(packets)
+        ws_app = 'wireshark'
+
+    ws_script = 'lua_script:nordic_ble.lua'
+    ws_filter = '!(btle.data_header.length==0)'
+    ws_cmd = '%s -X %s -Y "%s" -i "%s" -k' % (ws_app, ws_script, ws_filter, myPipe.getPipeName())
+
+    print("Run: " + ws_cmd)
+
+    try:
+        proc = subprocess.Popen(ws_cmd, shell = True)
+    except:
+        pass
+
+    print("Waiting for Wireshark...")
+    myPipe.open()
+    mySniffer.subscribe("NEW_BLE_PACKET", myPipe.newBlePacket)
 
 
 if __name__ == '__main__':
@@ -124,6 +175,12 @@ if __name__ == '__main__':
                            help="serial port location ('COM14', '/dev/tty.usbserial-DN009WNO', etc.)")
 
     # Optional arguments:
+    argparser.add_argument("-p", "--pipe",
+                           dest="pipe",
+                           action="store_true",
+                           default=False,
+                           help="Pipe packets to wireshark")
+
     argparser.add_argument("-l", "--logfile",
                            dest="logfile",
                            default=CaptureFiles.captureFilePath,
@@ -149,7 +206,7 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     # Display the libpcap logfile location
-    print "Capturing data to " + args.logfile
+    print("Capturing data to " + args.logfile)
     CaptureFiles.captureFilePath = args.logfile
 
     # Try to open the serial port
@@ -157,21 +214,21 @@ if __name__ == '__main__':
         setup(args.serialport)
     except OSError:
         # pySerial returns an OSError if an invalid port is supplied
-        print "Unable to open serial port '" + args.serialport + "'"
+        print("Unable to open serial port '" + args.serialport + "'")
         sys.exit(-1)
     except KeyboardInterrupt:
         sys.exit(-1)
 
-    # Optionally display some information about the sniffer
-    if args.verbose:
-        print "Sniffer Firmware Version: " + str(mySniffer.swversion)
+    # Display some information about the sniffer
+    print("Software Version: " + str(mySniffer.swversion))
+    print("Firmware Version: " + str(mySniffer.fwversion))
 
     # Scan for devices in range until the user makes a selection
     try:
         d = None
         """@type: Device"""
         if args.target:
-            print "specified target device", args.target
+            print("Specified target device " + args.target)
             _mac = map(lambda x: int(x, 16) , args.target.split(':'))
             if len(_mac) != 6:
                 raise ValueError("Invalid device address")
@@ -180,37 +237,42 @@ if __name__ == '__main__':
 
         # loop will be skipped if a target device is specified on commandline
         while d is None:
-            print "Scanning for BLE devices (5s) ..."
+            print("Scanning for BLE devices (5s) ...")
             devlist = scanForDevices()
             if len(devlist):
                 # Select a device
                 d = selectDevice(devlist)
 
         # Start sniffing the selected device
-        print "Attempting to follow device {0}:{1}:{2}:{3}:{4}:{5}".format("%02X" % d.address[0],
+        print("Attempting to follow device {0}:{1}:{2}:{3}:{4}:{5}".format("%02X" % d.address[0],
                                                                            "%02X" % d.address[1],
                                                                            "%02X" % d.address[2],
                                                                            "%02X" % d.address[3],
                                                                            "%02X" % d.address[4],
-                                                                           "%02X" % d.address[5])
-        # Make sure we actually followed the selected device (i.e. it's still available, etc.)
+                                                                           "%02X" % d.address[5]))
+        # TODO: Make sure we actually followed the selected device (i.e. it's still available, etc.)
         if d is not None:
-            mySniffer.follow(d)
-        else:
-            print "ERROR: Could not find the selected device"
+            if args.pipe:
+                setupPipe()
 
-        # Dump packets
-        while True:
-            dumpPackets()
-            time.sleep(1)
+            mySniffer.follow(d)
+
+        else:
+            print("ERROR: Could not find the selected device")
+
+        loop()
 
         # Close gracefully
         mySniffer.doExit()
+        if myPipe is not None:
+            myPipe.close()
         sys.exit()
 
     except (KeyboardInterrupt, ValueError, IndexError) as e:
         # Close gracefully on CTRL+C
         if 'KeyboardInterrupt' not in str(type(e)):
-            print "Caught exception:", e
+            print("Caught exception:" + str(e))
         mySniffer.doExit()
+        if myPipe is not None:
+            myPipe.close()
         sys.exit(-1)
